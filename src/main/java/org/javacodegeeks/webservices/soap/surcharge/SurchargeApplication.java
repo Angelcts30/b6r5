90a6d9a164a6b9ab46695e9b4f6be343e7cd3eae
Angelcts30 <angel.casans-98@gmail.com>
2023-02-28 18:28:36 +0100

    añadiendo el codigo de la rubrica

---
+++ src/main/java/org/javacodegeeks/webservices/soap/surcharge/SurchargeApplication.java
@@ -0,0 +1,15 @@
+package org.javacodegeeks.webservices.soap.surcharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SurchargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurchargeApplication.class, args);
+
+		System.out.println("SurchargeApplication now running....");
+	}
+}
